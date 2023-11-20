0d9f075037897f3e631b74dae08af923a009045b
Lukas Wunner <lukas.wunner@live.de>
2023-11-20 16:50:18 +0100

    change from reflection field access to harmony ref access. Restored a minor improvement when switching adjacent slots

--- ItemQuickSwitchMod/Patches/PlayerControllerBPatch.cs
+++ ItemQuickSwitchMod/Patches/PlayerControllerBPatch.cs
@@ -4,20 +4,19 @@ using System.Collections.Generic;
 using System.Reflection;
 using System;
 using UnityEngine.InputSystem;
-using System.Diagnostics;
-using BepInEx.Logging;
 using UnityEngine;
 
+// ReSharper disable InconsistentNaming
 namespace ItemQuickSwitchMod.Patches;
 
 [HarmonyPatch(typeof(PlayerControllerB))]
 internal class PlayerControllerBPatch
 {
     private static readonly Dictionary<string, MethodInfo> MethodCache = new();
-    private static readonly Dictionary<string, FieldInfo> FieldCache = new();
+    // private static readonly Dictionary<string, FieldInfo> FieldCache = new();
 
-    private static readonly object[] BackwardsParam = new object[1] { false };
-    private static readonly object[] ForwardsParam = new object[1] { true };
+    private static readonly object[] BackwardsParam = { false };
+    private static readonly object[] ForwardsParam = { true };
 
     private static object InvokePrivateMethod(PlayerControllerB instance, string methodName, object[] parameters = null)
     {
@@ -28,29 +27,34 @@ internal class PlayerControllerBPatch
         return method?.Invoke(instance, parameters);
     }
 
-    private static object GetPrivateField(PlayerControllerB instance, string fieldName)
-    {
-        FieldCache.TryGetValue(fieldName, out var field);
-        field ??= typeof(PlayerControllerB).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
-
-        FieldCache[fieldName] = field;
-        return field?.GetValue(instance);
-    }
+    // currently unused because switched to ref access:
+    //
+    // private static T GetPrivateFieldValue<T>(PlayerControllerB instance, string fieldName)
+    // {
+    //     FieldCache.TryGetValue(fieldName, out var field);
+    //     field ??= typeof(PlayerControllerB).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+    //
+    //     FieldCache[fieldName] = field;
+    //     return (T)field?.GetValue(instance);
+    // }
 
     [HarmonyPatch("Update")]
     [HarmonyPostfix]
-    public static void PlayerControllerB_Update(PlayerControllerB __instance)
+    public static void PlayerControllerB_Update(
+        PlayerControllerB __instance,
+        ref float ___timeSinceSwitchingSlots,
+        ref bool ___throwingObject
+    )
     {
-
         if ((!__instance.IsOwner || !__instance.isPlayerControlled ||
-            __instance.IsServer && !__instance.isHostPlayerObject) && !__instance.isTestingPlayer) return;
+             __instance.IsServer && !__instance.isHostPlayerObject) && !__instance.isTestingPlayer) return;
 
         var keyDown = Array.Find(CustomAction.AllActions, it =>
             Keyboard.current[it.ConfigEntry.Value].wasPressedThisFrame);
 
         if (keyDown == null) return;
 
-        switch(keyDown?.Id)
+        switch (keyDown.Id)
         {
             case "Emote1":
                 PerformEmote(__instance, 1);
@@ -60,7 +64,10 @@ internal class PlayerControllerBPatch
                 break;
             default:
                 StopEmotes(__instance);
-                SwitchItemSlots(__instance, keyDown.SlotNumber);
+                if (SwitchItemSlots(__instance, keyDown.SlotNumber, ___timeSinceSwitchingSlots, ___throwingObject))
+                {
+                    ___timeSinceSwitchingSlots = 0f;
+                }
                 break;
         }
     }
@@ -73,45 +80,53 @@ internal class PlayerControllerBPatch
         __instance.StartPerformingEmoteServerRpc();
     }
 
-    private static void SwitchItemSlots(PlayerControllerB __instance, int requestedSlot)
+    private static bool SwitchItemSlots(PlayerControllerB __instance, int requestedSlot, float timeSinceSwitchingSlots,
+        bool isThrowingObject
+    )
     {
-        if (!isItemSwitchPossible(__instance) || __instance.currentItemSlot == requestedSlot) return;
+        if (!IsItemSwitchPossible(__instance, timeSinceSwitchingSlots, isThrowingObject) ||
+            __instance.currentItemSlot == requestedSlot)
+        {
+            return false;
+        }
 
         var distance = __instance.currentItemSlot - requestedSlot;
+        var requestedSlotIsLowerThanCurrent = (distance > 0);
 
-        if (distance > 0)
+        if (Math.Abs(distance) == __instance.ItemSlots.Length - 1)
         {
-            var param = (distance > 0) ? BackwardsParam : ForwardsParam;
+            // we can just skip one slot forwards/backwards here and save RPC calls.
+            var parameterArray = requestedSlotIsLowerThanCurrent ? ForwardsParam : BackwardsParam;
+            InvokePrivateMethod(__instance, "SwitchItemSlotsServerRpc", parameterArray);
+        }
+        else
+        {
+            var parameterArray = requestedSlotIsLowerThanCurrent ? BackwardsParam : ForwardsParam;
             do
             {
-                InvokePrivateMethod(__instance, "SwitchItemSlotsServerRpc", param);
-                distance += (distance > 0) ? -1 : 1;
-            } while (distance > 0);
+                InvokePrivateMethod(__instance, "SwitchItemSlotsServerRpc", parameterArray);
+                distance += requestedSlotIsLowerThanCurrent ? -1 : 1;
+            } while (distance != 0);
         }
 
         ShipBuildModeManager.Instance.CancelBuildMode();
         __instance.playerBodyAnimator.SetBool("GrabValidated", false);
 
-        var switchSlotsTimerField = typeof(PlayerControllerB).GetField("timeSinceSwitchingSlots", BindingFlags.NonPublic | BindingFlags.Instance);
-        switchSlotsTimerField?.SetValue(__instance, 0.0f);
-
-        var switchItemParams = new object[] { requestedSlot, __instance.ItemSlots[requestedSlot] };
+        var switchItemParams = new object[] { requestedSlot, null };
         InvokePrivateMethod(__instance, "SwitchToItemSlot", switchItemParams);
 
         if (__instance.currentlyHeldObjectServer != null)
-        {
             __instance.currentlyHeldObjectServer.gameObject.GetComponent<AudioSource>()
                 .PlayOneShot(__instance.currentlyHeldObjectServer.itemProperties.grabSFX, 0.6f);
-        }
+
+        return true;
     }
 
-    private static bool isItemSwitchPossible(PlayerControllerB __instance)
+    private static bool IsItemSwitchPossible(PlayerControllerB __instance, float timeSinceSwitchingSlots,
+        bool isThrowingObject)
     {
-        var switchSlotsTimer = (float)GetPrivateField(__instance, "timeSinceSwitchingSlots");
-        var throwingObject = (bool)GetPrivateField(__instance, "throwingObject");
-
-        return !(switchSlotsTimer < 0.01 || __instance.inTerminalMenu || __instance.isGrabbingObjectAnimation ||
-                 __instance.inSpecialInteractAnimation || throwingObject || __instance.isTypingChat ||
+        return !(timeSinceSwitchingSlots < 0.01 || __instance.inTerminalMenu || __instance.isGrabbingObjectAnimation ||
+                 __instance.inSpecialInteractAnimation || isThrowingObject || __instance.isTypingChat ||
                  __instance.twoHanded || __instance.activatingItem || __instance.jetpackControls ||
                  __instance.disablingJetpackControls);
     }
