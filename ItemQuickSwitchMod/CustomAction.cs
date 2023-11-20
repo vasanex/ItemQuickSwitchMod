9b59ed29050087ef7a2df74a0780742493911d3c
Mark de Graaff <60252744+MarkdeGraaff@users.noreply.github.com>
2023-11-20 11:08:21 +0100

    Code refactoring by MarkdeGraaff
    * Refactored code
    
    * Committing changes before rebase
    
    ---------
    
    Co-authored-by: vasanex <150875294+vasanex@users.noreply.github.com>

--- ItemQuickSwitchMod/CustomAction.cs
+++ ItemQuickSwitchMod/CustomAction.cs
@@ -1,66 +1,30 @@
 using BepInEx.Configuration;
 using UnityEngine.InputSystem;
 
-namespace ItemQuickSwitchMod;
-
-public sealed class CustomAction
+namespace ItemQuickSwitchMod
 {
-    public static readonly CustomAction Emote1 = new(
-        "m_Movement_Emote1",
-        UnityEngine.InputSystem.Key.F1,
-        "Dance emote",
-        0
-    );
-
-    public static readonly CustomAction Emote2 = new(
-        "m_Movement_Emote2",
-        UnityEngine.InputSystem.Key.F2,
-        "Point emote",
-        0
-    );
-
-    public static readonly CustomAction Slot1 = new(
-        "m_Movement_Slot1",
-        UnityEngine.InputSystem.Key.Digit1,
-        "Equip Slot 1",
-        0
-    );
-
-    public static readonly CustomAction Slot2 = new(
-        "m_Movement_Slot2",
-        UnityEngine.InputSystem.Key.Digit2,
-        "Equip Slot 2",
-        1
-    );
-
-    public static readonly CustomAction Slot3 = new(
-        "m_Movement_Slot3",
-        UnityEngine.InputSystem.Key.Digit3,
-        "Equip Slot 3",
-        2
-    );
-
-    public static readonly CustomAction Slot4 = new(
-        "m_Movement_Slot4",
-        UnityEngine.InputSystem.Key.Digit4,
-        "Equip Slot 4",
-        3
-    );
-
-    public static readonly CustomAction[] AllActions = { Emote1, Emote2, Slot1, Slot2, Slot3, Slot4 };
-
-    public string Id { get; }
-    public Key Shortcut { get; }
-    public string Description { get; }
-    public int SlotNumber { get; }
-
-    public ConfigEntry<Key> ConfigEntry { get; set; }
-
-    private CustomAction(string id, Key config, string description, int slotNumber)
+    public sealed class CustomAction
     {
-        Id = id;
-        Shortcut = config;
-        Description = description;
-        SlotNumber = slotNumber;
+        public static readonly ActionItem Emote1 = new ActionItem("Emote1", Key.F1, "Dance emote", 0);
+        public static readonly ActionItem Emote2 = new ActionItem("Emote2", Key.F2, "Point emote", 0);
+        public static readonly ActionItem Slot1 = new ActionItem("Slot1", Key.Digit1, "Equip Slot 1", 0);
+        public static readonly ActionItem Slot2 = new ActionItem("Slot2", Key.Digit2, "Equip Slot 2", 1);
+        public static readonly ActionItem Slot3 = new ActionItem("Slot3", Key.Digit3, "Equip Slot 3", 2);
+        public static readonly ActionItem Slot4 = new ActionItem("Slot4", Key.Digit4, "Equip Slot 4", 3);
+
+        public static readonly ActionItem[] AllActions = { Emote1, Emote2, Slot1, Slot2, Slot3, Slot4 };
+
+        public class ActionItem
+        {
+            public string Id { get; }
+            public Key Shortcut { get; }
+            public string Description { get; }
+            public int SlotNumber { get; }
+
+            public ConfigEntry<Key> ConfigEntry { get; set; }
+
+            public ActionItem(string id, Key config, string description, int slotNumber)
+                => (Id, Shortcut, Description, SlotNumber) = (id, config, description, slotNumber);
+        }
     }
 }
---
+++ ItemQuickSwitchMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,125 @@
+﻿using HarmonyLib;
+using GameNetcodeStuff;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using UnityEngine.InputSystem;
+using System.Diagnostics;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace ItemQuickSwitchMod.Patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal class PlayerControllerBPatch
+{
+    private static readonly Dictionary<string, MethodInfo> MethodCache = new();
+    private static readonly Dictionary<string, FieldInfo> FieldCache = new();
+
+    private static readonly object[] BackwardsParam = new object[1] { false };
+    private static readonly object[] ForwardsParam = new object[1] { true };
+
+    private static object InvokePrivateMethod(PlayerControllerB instance, string methodName, object[] parameters = null)
+    {
+        MethodCache.TryGetValue(methodName, out var method);
+        method ??= typeof(PlayerControllerB).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+        MethodCache[methodName] = method;
+        return method?.Invoke(instance, parameters);
+    }
+
+    private static object GetPrivateField(PlayerControllerB instance, string fieldName)
+    {
+        FieldCache.TryGetValue(fieldName, out var field);
+        field ??= typeof(PlayerControllerB).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+        FieldCache[fieldName] = field;
+        return field?.GetValue(instance);
+    }
+
+    [HarmonyPatch("Update")]
+    [HarmonyPostfix]
+    public static void PlayerControllerB_Update(PlayerControllerB __instance)
+    {
+
+        if ((!__instance.IsOwner || !__instance.isPlayerControlled ||
+            __instance.IsServer && !__instance.isHostPlayerObject) && !__instance.isTestingPlayer) return;
+
+        var keyDown = Array.Find(CustomAction.AllActions, it =>
+            Keyboard.current[it.ConfigEntry.Value].wasPressedThisFrame);
+
+        if (keyDown == null) return;
+
+        switch(keyDown?.Id)
+        {
+            case "Emote1":
+                PerformEmote(__instance, 1);
+                break;
+            case "Emote2":
+                PerformEmote(__instance, 2);
+                break;
+            default:
+                StopEmotes(__instance);
+                SwitchItemSlots(__instance, keyDown.SlotNumber);
+                break;
+        }
+    }
+
+    private static void PerformEmote(PlayerControllerB __instance, int emoteId)
+    {
+        __instance.timeSinceStartingEmote = 0.0f;
+        __instance.performingEmote = true;
+        __instance.playerBodyAnimator.SetInteger("emoteNumber", emoteId);
+        __instance.StartPerformingEmoteServerRpc();
+    }
+
+    private static void SwitchItemSlots(PlayerControllerB __instance, int requestedSlot)
+    {
+        if (!isItemSwitchPossible(__instance) || __instance.currentItemSlot == requestedSlot) return;
+
+        var distance = __instance.currentItemSlot - requestedSlot;
+
+        if (distance > 0)
+        {
+            var param = (distance > 0) ? BackwardsParam : ForwardsParam;
+            do
+            {
+                InvokePrivateMethod(__instance, "SwitchItemSlotsServerRpc", param);
+                distance += (distance > 0) ? -1 : 1;
+            } while (distance > 0);
+        }
+
+        ShipBuildModeManager.Instance.CancelBuildMode();
+        __instance.playerBodyAnimator.SetBool("GrabValidated", false);
+
+        var switchSlotsTimerField = typeof(PlayerControllerB).GetField("timeSinceSwitchingSlots", BindingFlags.NonPublic | BindingFlags.Instance);
+        switchSlotsTimerField?.SetValue(__instance, 0.0f);
+
+        var switchItemParams = new object[] { requestedSlot, __instance.ItemSlots[requestedSlot] };
+        InvokePrivateMethod(__instance, "SwitchToItemSlot", switchItemParams);
+
+        if (__instance.currentlyHeldObjectServer != null)
+        {
+            __instance.currentlyHeldObjectServer.gameObject.GetComponent<AudioSource>()
+                .PlayOneShot(__instance.currentlyHeldObjectServer.itemProperties.grabSFX, 0.6f);
+        }
+    }
+
+    private static bool isItemSwitchPossible(PlayerControllerB __instance)
+    {
+        var switchSlotsTimer = (float)GetPrivateField(__instance, "timeSinceSwitchingSlots");
+        var throwingObject = (bool)GetPrivateField(__instance, "throwingObject");
+
+        return !(switchSlotsTimer < 0.01 || __instance.inTerminalMenu || __instance.isGrabbingObjectAnimation ||
+                 __instance.inSpecialInteractAnimation || throwingObject || __instance.isTypingChat ||
+                 __instance.twoHanded || __instance.activatingItem || __instance.jetpackControls ||
+                 __instance.disablingJetpackControls);
+    }
+
+    private static void StopEmotes(PlayerControllerB __instance)
+    {
+        __instance.performingEmote = false;
+        __instance.StopPerformingEmoteServerRpc();
+        __instance.timeSinceStartingEmote = 0.0f;
+    }
+}
